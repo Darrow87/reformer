77af81b01fb623eaa1aa9023a5b5e2d17738195a
Alex Forger <alexforgerii@gmail.com>
2024-02-06 05:48:22 -1000

    add controllers and view for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    # GET /users/new
+    # This action is typically used to display a form for creating a new user.
+    def new
+    end
+  
+    # POST /users
+    # This action is used to handle the form submission, attempting to create a new user with the provided parameters.
+    def create
+    end
+  
+  end
+  
